62805bb34eadd0c78c976e370ce3774e679255b7
Zecorr <zecorr88@gmail.com>
2023-02-27 20:55:32 -0300

    refactor with ternary operator

--- return_negative.js
+++ return_negative.js
@@ -24,3 +24,11 @@ function makeNegative(num) {
     return -num;
   }
 }
+
+// Solution 2: refactor with ternary operator
+function makeNegative(num) {
+  if (num === 0) {
+    return num;
+  }
+  return num < 0 ? num : -num;
+}
