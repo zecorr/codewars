85fbe18a76985c1b504d56534540cf2f7fbbe7bb
Zecorr <zecorr88@gmail.com>
2023-02-27 10:33:43 -0300

    setup even or odd challenge

---
+++ even_or_odd.js
@@ -0,0 +1,6 @@
+// ::::: Instructions :::::
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// function evenOrOdd(number) {
+//
+// }
