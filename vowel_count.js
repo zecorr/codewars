8ef774267a81901333a52efbd8c212bf8d49aa4b
Zecorr <zecorr88@gmail.com>
2023-03-01 15:07:23 -0300

    solve vowel count

--- vowel_count.js
+++ vowel_count.js
@@ -8,3 +8,21 @@
 // function getCount(str) {
 //   return 0;
 // }
+
+// Solution 1:
+function getCount(str) {
+  let counter = 0;
+  const s = str.split("");
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s[i] === "a" ||
+      s[i] === "e" ||
+      s[i] === "i" ||
+      s[i] === "o" ||
+      s[i] === "u"
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+}
